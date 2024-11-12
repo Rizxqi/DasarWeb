3a8676825451eeff8ef2aacfdf8a7abd0dd1a5b1
rizxqi <rizqidewanto23@gmail.com>
2024-11-12 10:54:10 +0700

    Jobsheet 12 Praktikum 1

---
+++ JS12_PHPOOP/oop.php
@@ -0,0 +1,20 @@
+<?php
+class Car 
+{
+    public $brand;
+
+    public function startEngine()
+    {
+        echo "Engine started!";
+    }
+}
+
+$car1 = new car();
+$car1->brand = "Toyota";
+
+$car2 = new car();
+$car2->brand = "Honda";
+
+$car1->startEngine();
+echo $car2->brand;
+
